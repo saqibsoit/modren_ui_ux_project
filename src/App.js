f6850e79a478362ec8350f08a75f4352ff08e091
saqibsoit <51706503+saqibsoit@users.noreply.github.com>
2023-07-28 16:15:22 +0500

    files and folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+
+import{ Footer, Blog, Features, Header, Possibility, WhatGPT3} from  './containers';
 import {Article ,Brand, Cta, Feature, Navbar} from './components'
 
 
 const App= ()=> {
   return (
     <div className="App">
-      <h1>GPT-3</h1>
+      <div className="gradient__bd">
+        <Navbar/>
+        <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <Cta />
+        <Blog />
+        <Footer />
     </div>
   );
 }
